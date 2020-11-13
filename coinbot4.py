50abe858dc5e4462ba163894cc73c7a6b1c4a2e6
friendlynokill <74374768+friendlynokill@users.noreply.github.com>
2020-11-13 10:15:55 -0800

    Update coinbot4.py

--- coinbot4.py
+++ coinbot4.py
@@ -39,7 +39,7 @@ async def on_ready():
     activity = discord.Game(name="!help for commands", type = 3)
     await bot.change_presence(status=discord.Status.online, activity=activity)
     print(f'{bot.user.name} has logged in!')
-    await message.channel.send(bot.user.name ' has logged in!')
+    await message.channel.send(bot.user.name, ' has logged in!')
     
 @bot.event
 async def on_message(message):
